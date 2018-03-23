ae390cc8a66ebe14cf366116a695337667edb1db
Ktvipers <kyletashiro@gmail.com>
2018-03-23 14:00:42 -0700

    ching

--- app/src/main/java/com/example/evan/languageapp/HomeScreen.java
+++ app/src/main/java/com/example/evan/languageapp/HomeScreen.java
@@ -1,10 +1,10 @@
 package com.example.evan.languageapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -122,6 +123,13 @@ public class HomeScreen extends AppCompatActivity
 
     @Override
     public void onClick (View view){
+        lessons.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomeScreen.this, LessonsActivity.class);
+                startActivity(i);
 
+            }
+        });
     }
 }
