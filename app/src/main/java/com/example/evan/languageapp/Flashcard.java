ccf36d1d3782f7729c3946c14aeedb094fa92ac5
Ktvipers <kyletashiro@gmail.com>
2018-05-03 13:01:26 -0700

    All lessons completed

--- app/src/main/java/com/example/evan/languageapp/Flashcard.java
+++ app/src/main/java/com/example/evan/languageapp/Flashcard.java
@@ -84,7 +84,7 @@ public class Flashcard {
         Flashcard E10 = new Flashcard("我愛你 wǒ ài nǐ", "I love you");
 
 
-        Flashcard[] lessonOne = {A1, A2, A3, A4, A5, A6, A7, A8, A9, A10};
+        Flashcard[] lessonOne = {A1, A2, A3, A4, A5, A6, A7, A8, A9, A10};                             //Lesson One
         Flashcard[] lessonTwo = {B1, B2, B3, B4, B5, B6, B7, B8, B9, B10};
         Flashcard[] lessonThree = {C1, C2, C3, C4, C5, C6, C7, C8, C9, C10};
         Flashcard[] lessonFour = {D1, D2, D3, D4, D5, D6, D7, D8, D9, D10};
